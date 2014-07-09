4613d8d4f677b3895e5697e6a1913b7b4f60eccc
Gabe Fierro <gtfierro225@gmail.com>
2014-07-09 09:59:46 -0700

    update novelty file

--- novelty.py
+++ novelty.py
@@ -1,43 +1,81 @@
 import pandas as pd
+import sys
 
 def trim(txt):
   if '.0' in txt:
     return txt[:txt.rfind('.0')]
   return txt
 
-matrix = pd.read_csv('matrix',header=None)
+# 0: patent idx 1: word idx 2: word count
+matrix = pd.read_csv('full_matrix',header=None)
+# 0: patent 1: date 2: sortable date
 dates = pd.read_csv('appdates.csv',header=None)
+wordcounts = pd.DataFrame()
+wordcounts[0] = matrix.groupby(0).count()[1].astype('float32')
 
-patent = 7692844
+# patent is a patent number, e.g. '4130416', NOT a row index
+#patent = sys.argv[1]
+for patent in sys.argv[1:]:
+# get the date for that patent
+  daterow = dates[dates[0] == patent]
+  date = int(daterow[2])
 
-daterow = dates[dates[0] == patent]
-date = int(daterow[2])
-index = int(daterow.index)
+  # get all words + word counts for that patent
+  rows = matrix[matrix[0] == int(daterow.index)]
+  # number of unique words in this patent
+  unique_count_focal = len(rows)
 
-rows = matrix[matrix[0] == index]
-# number of unique words in the focal patent
-focaluniquewords = len(rows)
+  # get all patents that share at least 1 word
+  overlap_patents = matrix[matrix[1].isin(rows[1])]
 
-words = matrix[matrix[1].isin(rows[1])]
+  # get the patent numbers for patents before/after the date of our focal patent
+  before = dates[((date - dates[2]) < 50000) & ((date - dates[2]) > 0)]
+  after = dates[((dates[2] - date) < 50000) & ((dates[2] - date) > 0)]
 
-before = dates[((date - dates[2]) < 50000) & ((date - dates[2]) > 0)]
-after = dates[((dates[2] - date) < 50000) & ((dates[2] - date) > 0)]
+  pat_before = overlap_patents[overlap_patents[0].isin(before.index)]
+  pat_after = overlap_patents[overlap_patents[0].isin(after.index)]
 
-pat_before = words[words[0].isin(before.index)].groupby(0).count()[1]
-pat_after = words[words[0].isin(after.index)].groupby(0).count()[1]
 
-after['count'] = pat_after
-before['count'] = pat_before
+  pat_before_count = pat_before.groupby(0).count()[1].astype('float32')
+  pat_after_count = pat_after.groupby(0).count()[1].astype('float32')
 
-after = after.fillna(0)
-before = before.fillna(0)
+  #import IPython
+  #IPython.embed(user_ns=locals())
 
-after[0] = after[0].astype(str).apply(trim)
-after[2] = after[2].astype(str).apply(trim)
-before[2] = before[2].astype(str).apply(trim)
+  try:
+    after['overlap'] = pat_after_count
+    t1 = list((unique_count_focal - pat_after_count).values)
+    t2 = list(wordcounts[wordcounts.index.isin(pat_after[0])].values)
+    print len(t1),len(t2), len(after)
+    after['union'] = [a+b for a,b in zip(t1,t2)]
+    after['union'] = after['union'].astype(int)
+    after['jaccard'] = after['overlap'] / after['union']
+    after = after.sort('jaccard', ascending=False)
 
-del after[2]
-del before[2]
+    before['overlap'] = pat_before_count
+    t1 = list((unique_count_focal - pat_before_count).values)
+    t2 = list(wordcounts[wordcounts.index.isin(pat_before[0])].values)
+    print len(t1),len(t2),len(before)
+    before['union'] = [a+b for a,b in zip(t1,t2)]
+    before['union'] = before['union'].astype(int)
+    before['jaccard'] = before['overlap'] / before['union']
+    before = before.sort('jaccard', ascending=False)
+  except Exception as e:
+    import IPython
+    IPython.embed(user_ns=locals())
+    print 'ERROR',patent,e
 
-after.to_csv('after.csv',index=False)
-before.to_csv('before.csv',index=False)
+  after = after.fillna(0)
+  before = before.fillna(0)
+  print after.columns
+
+  # 0: patent num, 1: date, 2: sortabledate, overlap: # overlapping words, union: # unique words
+
+  after[0] = after[0].astype(str).apply(trim)
+  before[0] = before[0].astype(str).apply(trim)
+
+  del after[2]
+  del before[2]
+
+  after.to_csv('{0}_after.csv'.format(patent),index=False)
+  before.to_csv('{0}_before.csv'.format(patent),index=False)
